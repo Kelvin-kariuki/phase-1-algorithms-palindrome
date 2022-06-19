f995f91e1a710211b8bd9eb88dacf2120411ab76
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-19 12:34:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let array = word.split("")
+  let reversedArray = array.reverse()
+  let reversedWord = reversedArray.join("")
+  if (word == reversedWord){
+    return true;
+  } else {
+    return false;
+  }
 }
+isPalindrome('abba')
+isPalindrome('racecar')
+
 
 /* 
   Add your pseudocode here
+  Create a variable to hold word
+  Reverse the variable
+  use if statement for comparision
 */
 
 /*
   Add written explanation of your solution here
+  Create a variable and reverse the string inside the variable. Use if statement to check if true else output false
 */
 
 // You can run `node index.js` to view these console logs
